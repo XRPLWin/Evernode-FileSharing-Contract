901f367fb9c0697a3e21c54b1c1fc8607e6a377b
zgrguric <zgrguric@xrplwin.com>
2024-10-08 23:42:24 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# evernode-filesharing-contract
+# Evernode File Sharing Contract
+It allows you to upload and share link to download large file. 
+
+This contract consist of two components, client loader (HTML/JS static) and contract code.
+
+# Run client
+
+Find /client/index.html and serve it locally in secure context or host it on your domain, yes just that file (low bandwith). This is only file required to load your smart contract to the web browser.
+
+In `index.html` there is `const ws_endpoint = 'wss://localhost:8081';` which you can change to point to your instance in production.
+
+# Run contract locally
+
+```
+npm start
+```
+(requires Docker - see Evernode official documentation)
+
+# What does it do?
+
+It allows you to upload and share link to download large file.  
+File is chunked in web browser and chunks are uploaded to the contract. Same goes to download, it downloads chunks and web browser rearranges chunks to whole file.
+
+Additional implemented options is to track metadata of uploaded file where you can add additional context, in this contract we are using password protection and adding note with uploaded file.
+
+# The tech and showcase
+
+This contract in whole is showcasing few things:  
+1. Keeping website logic (layout, javascript, ...) INSIDE contract
+2. Using static loader file (index.html) to load contract - it can be reused for many types of contract which follow same convention
+3. Chunking and rearranging files in web browser (working with buffers...)
+4. Saving and serving files from Evernode Contract
+5. Serving various request from Evernode Contract
+
+## Author
+XRPLWin (xrplwin.com)
---
+++ src/filesharingdapp.js
@@ -0,0 +1,121 @@
+const fs = require('fs');
+const crypto = require('crypto')
+
+const htmlFile = 'template/app.html';
+const uploadDir = '../uploadedfiles';
+
+export class filesharingdapp {
+    sendOutput;     // This function must be wired up by the caller.
+    sendRawOutput;  // This function must be wired up by the caller.
+
+    async handleRequest(user, message, isReadOnly) {
+
+        // Serve HTML of this DApp
+        if(message.type == 'gethtml') {
+            await this.sendOutput(user,{
+                type: 'html',
+                data: (fs.readFileSync(htmlFile)).toString()
+            });
+        } else if (message.type == 'download-metadata') {
+            if(message.uploaderpubkey && message.fileid) {
+                const userID = message.uploaderpubkey;
+                const metafile = uploadDir+'/'+userID+'/'+parseInt(message.fileid)+'/metadata.json';
+                let metadataContents = (fs.readFileSync(metafile)).toString();
+                //parse metadataContents and remove all properties starting with "_" - those are protected and should not be exposed
+                //todo
+                await this.sendOutput(user,{
+                    type: "json",
+                    data: metadataContents
+                });
+            }
+        } else if (message.type == 'download') {
+            if(message.uploaderpubkey && message.fileid && message.part) {
+                const userID = message.uploaderpubkey;
+                //Get Metafile to check if this file is password protected
+                const metafile = uploadDir+'/'+userID+'/'+parseInt(message.fileid)+'/metadata.json';
+                let metadataContents = JSON.parse((fs.readFileSync(metafile)).toString());
+
+                let allowDownload = true;
+                if(metadataContents.access == 'password') {
+                    // Password is required
+                    allowDownload = false;
+                    if(message.password) {
+                        // Password was sent
+                        let sentPasswordHash = crypto.createHash('md5').update(message.password).digest("hex");
+                        if(sentPasswordHash == metadataContents._access_password) { //compare
+                            allowDownload = true;
+                        }
+                    }
+                }
+
+                if(allowDownload) {
+                    const chunkfile = uploadDir+'/'+userID+'/'+parseInt(message.fileid)+'/'+parseInt(message.part)+'.chunk';
+                    let chunkContents = (fs.readFileSync(chunkfile));
+                    await this.sendOutput(user,{
+                        type:"filepart",
+                        data: chunkContents
+                    });
+                } else {
+                    await this.sendOutput(user, {
+                        type: 'error',
+                        error: 'Forbidden - password is invalid'
+                    })
+                }
+            }
+        } else if (message.type == 'upload-metadata') {
+            if (isReadOnly) {
+                await this.sendOutput(user, {
+                    type: 'error',
+                    error: 'Not supported in readonly mode'
+                })
+            } else if(message.fileid && message.contents) {
+                const userID = user.publicKey;
+                const dir = uploadDir+'/'+userID+'/'+parseInt(message.fileid);
+                if (!fs.existsSync(dir)) {
+                    fs.mkdirSync(dir, { recursive: true });
+                }
+                
+                if (!fs.existsSync(dir+'/metadata.json')) {
+                    fs.writeFileSync(dir+'/metadata.json',message.contents);
+                }
+                await this.sendRawOutput(user, {
+                    type: 'success',
+                    error: 'Metadata file stored'
+                })
+            }
+        } else if (message.type == 'upload') {
+           
+            if (isReadOnly) {
+                await this.sendOutput(user, {
+                    type: 'error',
+                    error: 'Not supported in readonly mode'
+                })
+            } else if(message.fileid && message.totalchunks && message.chunkno && message.chunk) {
+                //Store file chunk
+                const userID = user.publicKey;
+                const dir = uploadDir+'/'+userID+'/'+parseInt(message.fileid);
+
+                if (!fs.existsSync(dir)) {
+                    fs.mkdirSync(dir, { recursive: true });
+                }
+
+                //store file chunk
+                let chunkFilepath = dir+'/'+message.chunkno+'.chunk';
+
+                if (!fs.existsSync(chunkFilepath)) {
+                    fs.writeFileSync(chunkFilepath,Buffer.from(message.chunk, "hex"));
+                }
+                await this.sendRawOutput(user, {
+                    type: 'success',
+                    error: 'Chunk '+message.chunkno+' stored'
+                })
+            }
+        }
+        else {
+            await this.sendOutput(user, {
+                type: 'error',
+                error: 'Unknown message type'
+            })
+        }
+    }
+}
